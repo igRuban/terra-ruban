d79c9e1c9598a814e9a247537d360327b1b6aa13
Ruban <126575183+igRuban@users.noreply.github.com>
2023-11-08 02:04:28 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,6 +3,12 @@ provider "aws" {
   access_key = "AKIAWOPKGGK7UQC4VSXG"
   secret_key = "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"
 }
-
+resource "aws_instance" "foo" {
+  ami           = "ami-02e94b011299ef128"
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
 
 
