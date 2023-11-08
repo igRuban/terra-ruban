d7e725544b6dde8e55e4e95117f02466b85077e0
Ruban <126575183+igRuban@users.noreply.github.com>
2023-11-08 22:37:40 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,4 @@
 provider "aws" {
-  region     = "ap-south-1"
-  access_key = "AKIAWOPKGGK7UQC4VSXG"
-  secret_key = "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"
 }
 resource "aws_instance" "foo" {
   ami           = "ami-02e94b011299ef128"
